e750d7d19e4b639b2e10d57bb5950df0008e6cd2
Luciano <luciano.bestia@gmail.com>
2020-02-29 03:38:34 +0700

    server

--- webfolder/mem6/service_worker.js
+++ webfolder/mem6/service_worker.js
@@ -6,7 +6,7 @@
 //But the new service worker will not be activated until all 
 //tabs with this webapp are closed.
 
-const CACHE_NAME = '2020.229.311';
+const CACHE_NAME = '2020.229.325';
 
 self.addEventListener('install', event => {
     console.log('event install ', CACHE_NAME);
